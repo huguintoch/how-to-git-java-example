1468fa3c69b7d3c24c24d7157c91a745a1905b5e
Benjamín Gutierrez Mendoza <benjamingutierrezmendoza@MacBook-Pro-de-Benjamin.local>
2021-10-08 17:44:47 -0500

    Se modifico la clase del cuadrado

--- src/Cuadrado.java
+++ src/Cuadrado.java
@@ -1,16 +1,22 @@
 // [ToDo]: Code this class by adding constructor and methods
+    public class Cuadrado implements Figura {
 
-public class Cuadrado {
-
-    protected double largo, ancho;
-
-    // Constructor
-
-    public Cuadrado (double lado) {
-        this.largo = lado;
-        this.ancho = lado;
-    }
-
-    // Methods
+        protected double largo, ancho;
     
-}
+        public Cuadrado(double largo, double ancho) {
+            this.largo = largo;
+            this.ancho = ancho;
+        }
+    
+        public Rectangulo() {
+            this(5.0, 2.0);
+        }
+    
+        public String toString() {
+            return "Cuadrado de largo " + this.largo + " y ancho " + this.ancho;
+        }
+    }
+    
+
+
+    
