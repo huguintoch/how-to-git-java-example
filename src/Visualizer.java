ce6ff183577cc72d9c2e47d1919e00b6a56da1d3
Hugo Valdez <hugoisaac99@outlook.com>
2021-10-07 20:33:22 -0500

    Remove cuadrado

--- src/Visualizer.java
+++ src/Visualizer.java
@@ -1,7 +1,7 @@
 public class Visualizer {
     public static void main(String[] args) {
 
-        Figura[] figuras = { new Rectangulo(6.0, 4.0), new Cuadrado(), new Caja(10.0, 15.3, 20.0), new Caja() };
+        Figura[] figuras = { new Rectangulo(6.0, 4.0), new Caja(10.0, 10.0, 10.0), };
 
         for (int i = 0; i < figuras.length; i++) {
             System.out.println(figuras[i]);
